25d432a249d1ad5c4a577c7332a2652b6582d71a
Jorge Barrios <jorgebarmza@gmail.com>
2019-02-03 00:17:41 +0800

    read files

--- atd.for
+++ atd.for
@@ -1,18 +1,17 @@
       program atd
-      integer ios
       character employees*13, attendance*14, monthlyattendance*22
-      character date_n*10, date_e*18, attendant*100
-      parameter (u=20)
+      character atts(10000)*100, empl(10000)*100
+      character m_empl(10000)*100
+      character date_n*10, date_e*18, att*100
 
 c ******** main **********************************
 
       call read_args_and_open_files()
       read(11, '(a)') date_n
       call write_summary_header(date_e(date_n))
-
-      read(11, '(A)', IOSTAT=ios) attendant
-      write(*,*) attendant
-
+      call read_f(10, empl)
+      call read_f(11, atts)
+      call read_f(12, m_empl)
       end
 
 c ******** helper functions **********************
@@ -20,7 +19,7 @@ c ******** helper functions **********************
 c convert numeral date to english
       character*18 function date_e(date_n)
       character date_n*10
-c date_n is formatted "yyy-mm-dd"
+c date_n is formatted "yyyy-mm-dd"
       character year*4, month*2, day*2, m_e*9
       year = date_n(1:4)
       month = date_n(6:7)
@@ -41,6 +40,15 @@ c date_n is formatted "yyy-mm-dd"
 c todo trim day
       date_e = m_e // ' ' // day // ', ' // year
       return
+      end
+
+      subroutine read_f (u,arr)
+      character arr(10000)*100
+      integer id, u
+      id = 1
+ 10   read(u, '(A)', IOSTAT=ios) arr(id)
+      if(ios.LT.0) return
+      go to 10
       end
 
       subroutine write_summary_header (date_e)
