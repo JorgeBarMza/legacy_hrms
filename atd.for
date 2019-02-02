bc625d16d2e71b2a4abc2c635da0a91858567563
Jorge Barrios <jorgebarmza@gmail.com>
2019-02-02 14:35:11 +0800

    read fortran files

--- atd.for
+++ atd.for
@@ -1,9 +1,23 @@
-      program hello_world
-      implicit none
-c
-      character*32 text
-c
-      text = 'Hello World'
-      write (*,*) text
-c
+      program atd
+      integer u
+      character(4) s
+      character(13) employees
+      character(14) attendance
+      character(22) monthlyattendance
+      parameter (u=20)
+
+      CALL getarg(1, employees)
+      CALL getarg(2, attendance)
+      CALL getarg(3, monthlyattendance)
+
+      open (10, FILE=employees)
+      open (10, FILE=attendance)
+      open (10, FILE=monthlyattendance)
+      open (20, FILE='summaryfor.txt')
+
+      read(10, '(a)') s
+c      write(11,*) s
+      write(20,*) employees
+      write(20,*) attendance
+      write(20,*) monthlyattendance
       end
