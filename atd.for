fda9502c24ae78cea0910dfb77c0e676a27493e7
Jorge Barrios <jorgebarmza@gmail.com>
2019-02-03 15:58:18 +0800

    add report.pdf

--- atd.for
+++ atd.for
@@ -1,3 +1,19 @@
+* CSCI3180 Principles of Programming Languages
+*
+* --- Declaration ---
+*
+* I declare that the assignment here submitted is original except
+* for source material explicitly acknowledged. I also
+* acknowledge that I am aware of University policy and
+* regulations on honesty in academic work, and of the
+* disciplinary guidelines and procedures applicable to breaches
+* of such policy and regulations, as contained in the website
+* http://www.cuhk.edu.hk/policy/academichonesty/
+*
+* Assignment 1
+* Name : Jorge Alberto Barrios Mendoza
+* Student ID : 1155128883
+* Email Addr : 1155128883@link.cuhk.edu.hk
       program atd
       character employees*13, attendance*14, monthlyattendance*22
       character atts(10000)*100, empl(10000)*100
